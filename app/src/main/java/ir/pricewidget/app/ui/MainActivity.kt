b9297e69ac95469e74104b0ae8a04f464104e9f8
shahinsafaei <safaeishahin@gmail.com>
2026-09-22 02:15:26 +0330

    fix: add material-icons-extended dependency

--- app/src/main/java/ir/pricewidget/app/ui/MainActivity.kt
+++ app/src/main/java/ir/pricewidget/app/ui/MainActivity.kt
@@ -25,6 +25,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
